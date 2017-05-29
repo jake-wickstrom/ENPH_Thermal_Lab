7d2341fcf8d97385f062a3bc9c0d005023820bbb
Jake Wickstrom <jtwickstrom@alumni.ubc.ca>
2017-05-29 15:00:36 -0700

    Added some comments

--- MeasureControl.m
+++ MeasureControl.m
@@ -1,17 +1,27 @@
 clear
 a = arduino();
 
-volts = zeros([6 200]);
-calibrationData = zeros([6 200])
+%CONSTANTS
+CALIBRATION_LENGTH = 300;
+scaleFactor2 = [1.0307,0.9819,0.98946,0.98874,1.0278,0.98146]; %experimentally determined scaling coefficients
+
+volts = zeros([6 2000]);
+calibrationData = zeros([6 CALIBRATION_LENGTH]);
 
 %% Temperature Calibration
 
+%Calibration begin message. Waits until ok is pressed
 uiwait(msgbox('Ready to calibrate. Place all temperature sensors together and press OK','Calibrating','modal'))
+
+%Calibration warning message
 h = msgbox('Calibrating... Please do not touch sensors.');
+%Gets children of UI element and delete the ok button so warning stays on
+%screen during 
 child = get(h,'Children');
 delete(child(1))
 
-for i = 1:200
+%% Calibration loop
+for i = 1:CALIBRATION_LENGTH
     calibrationData(1,i) = readVoltage(a,'A0');
     calibrationData(2,i) = readVoltage(a,'A1');
     calibrationData(3,i) = readVoltage(a,'A2');
@@ -25,18 +35,20 @@ meanVoltage = mean2(calibrationData)
 
 scaleFactor = mean(calibrationData,2)./meanVoltage
 
-delete(h)
+%create figure to dipslay plots
+f = figure;
 
-figure
-
-for i = 1:200
-    volts(1,i) = readVoltage(a,'A0');
-    volts(2,i) = readVoltage(a,'A1');
-    volts(3,i) = readVoltage(a,'A2');
-    volts(4,i) = readVoltage(a,'A3');
-    volts(5,i) = readVoltage(a,'A4');
-    volts(6,i) = readVoltage(a,'A5');
+%% Data Loop 
+for i = 1:2000
+    %record voltage values from sensors
+    volts(1,i) = readVoltage(a,'A0')/scaleFactor(1);
+    volts(2,i) = readVoltage(a,'A1')/scaleFactor(2);
+    volts(3,i) = readVoltage(a,'A2')/scaleFactor(3);
+    volts(4,i) = readVoltage(a,'A3')/scaleFactor(4);
+    volts(5,i) = readVoltage(a,'A4')/scaleFactor(5);
+    volts(6,i) = readVoltage(a,'A5')/scaleFactor(6);
     
+    %display voltage values on plot
     plot(volts(1,:).*scaleFactor(1))
     hold;
     plot(volts(2,:).*scaleFactor(2))
@@ -44,13 +56,14 @@ for i = 1:200
     plot(volts(4,:).*scaleFactor(4))
     plot(volts(5,:).*scaleFactor(5))
     plot(volts(6,:).*scaleFactor(6))
+    legend('1','2','3','4','5','6')
     hold;
     pause(.05);
-end
-
-volts_avg = zeros([6 1]);
-
-for i=1:6
-    volts_avg(i) = mean(volts(i));
+    
+    %if the figure has been closed, break out of the temperature
+    %measurement loop
+    if not(ishandle(f))
+        break
+    end  
 end
 
