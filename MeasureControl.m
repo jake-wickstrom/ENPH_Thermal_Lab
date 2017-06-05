c74575d41818ebae9fe4d775c1c35cc4e7aea768
Cam <newtoncameron@hotmail.com>
2017-06-05 16:44:27 -0700

    New measure control and Temperature simulation

--- MeasureControl.m
+++ MeasureControl.m
@@ -1,68 +1,3 @@
-l = 0.3; %Length of rod
-k = 205; %Conductivity
-c = 900; %Heat capacity
-p = 2700; %density
-r = 0.01 ; %Radius
-k_c = 5; 
-sigma = 5.67*(10^-8); %Stefan-Boltzman constant
-P = 9.6; %Power
-
-delta_x = 0.01; %Distance step
-N = l / delta_x; %Total Intervals
-delta_t = 0.1; %Time step
-%T_heat = 50; %Heating Temperature
-T_amb = 20; %Ambient Temperature
-
-T_coeff = delta_t/(c * p * pi * (r^2) * delta_x);
-
-time = 36000;
-
-x = linspace(0,l,((l/delta_x))); %Distance vector
-x_mid = x+(delta_x/2); %Midpoint vector
-
-Temp = zeros([time ((l/delta_x))]); %Temperature vector
-Temp_change = zeros([1 ((l/delta_x))]); %Temperature change vector
-Temp =Temp +T_amb;
-
- for i = 2:time
-     
-    if mod(floor(i/3000)+1,2)== 0
-        P = 0;
-    else
-        P = 9.6;
-    end
-
-    %Mid points
-    Temp_change(2:N-1) = (k*delta_t/(c*p))*((Temp(i-1,1:N-2)-2*Temp(i-1,2:N-1)+Temp(i-1,3:N))/(delta_x^2));
-        
-    %End points
-    Temp_change(1) = Temp_change(1) +((-k/(c*p*(delta_x^2)))*(Temp(i-1,1) - Temp(i-1,2))*(delta_t)); %Transfer to next slice, 1 - 2
-    Temp_change(1) = Temp_change(1) +((T_coeff)*P); %Gain from power source
-    
-    Temp_change(30) = (((k/(c*p*(delta_x^2)))*(Temp(i-1,29) - Temp(i-1,30))*(delta_t))); %Transfer from slice before
-    
-    Temp_change = Temp_change - (T_coeff)*(2*pi*r*delta_x)*(k_c*(Temp(i-1,:)-T_amb)+(sigma*((Temp(i-1,:)+273).^4-(T_amb+273)^4))); %Convective and radiative loss
-     
-    Temp(i,:) = Temp_change + Temp(i-1,:);
-
-    %Plot settings
-        
-    %pause(.01);
- end
-
-figure
-hold on
-plot(Temp(:,3));
-plot(Temp(:,8));
-plot(Temp(:,11));
-plot(Temp(:,19));
-plot(Temp(:,28));
-
-figure
-plot(Temp(time,:));
-title('Temperature vs Distance');
-
-
 %% Data Loop 
 clear
 a = arduino();
@@ -102,4 +37,3 @@ for i = 1:time
         csvwrite(filename,volts);
     end
 end
-
---
+++ TempSim.m
@@ -0,0 +1,66 @@
+function [ Temp ] = TempSim( Power , time, frequency )
+%{Pass this function a Power (in Watts) and time and the function will simulate
+%}
+l = 0.3; %Length of rod
+k = 205; %Conductivity
+c = 900; %Heat capacity
+p = 2700; %density
+r = 0.01 ; %Radius
+k_c = 5; 
+sigma = 5.67*(10^-8); %Stefan-Boltzman constant
+
+delta_x = 0.01; %Distance step
+N = l / delta_x; %Total Intervals
+delta_t = 0.1; %Time step
+%T_heat = 50; %Heating Temperature
+T_amb = 20; %Ambient Temperature
+
+T_coeff = delta_t/(c * p * pi * (r^2) * delta_x);
+
+x = linspace(0,l,((l/delta_x))); %Distance vector
+
+Temp = zeros([time ((l/delta_x))]); %Temperature vector
+Temp_change = zeros([1 ((l/delta_x))]); %Temperature change vector
+Temp =Temp +T_amb;
+
+ for i = 2:time
+     
+    if mod(floor(i/frequency)+1,2)== 0
+        P = 0;
+    else
+        P = Power;
+    end
+
+    %Mid points
+    Temp_change(2:N-1) = (k*delta_t/(c*p))*((Temp(i-1,1:N-2)-2*Temp(i-1,2:N-1)+Temp(i-1,3:N))/(delta_x^2));
+        
+    %End points
+    Temp_change(1) = Temp_change(1) +((-k/(c*p*(delta_x^2)))*(Temp(i-1,1) - Temp(i-1,2))*(delta_t)); %Transfer to next slice, 1 - 2
+    Temp_change(1) = Temp_change(1) +((T_coeff)*P); %Gain from power source
+    
+    Temp_change(30) = (((k/(c*p*(delta_x^2)))*(Temp(i-1,29) - Temp(i-1,30))*(delta_t))); %Transfer from slice before
+    
+    Temp_change = Temp_change - (T_coeff)*(2*pi*r*delta_x)*(k_c*(Temp(i-1,:)-T_amb)+(sigma*((Temp(i-1,:)+273).^4-(T_amb+273)^4))); %Convective and radiative loss
+     
+    Temp(i,:) = Temp_change + Temp(i-1,:);
+
+    %Plot settings
+        
+    %pause(.01);
+ end
+
+figure
+hold on
+plot(Temp(:,3));
+plot(Temp(:,8));
+plot(Temp(:,11));
+plot(Temp(:,19));
+plot(Temp(:,28));
+
+figure
+plot(Temp(time,:));
+title('Temperature vs Distance');
+
+
+
+end
