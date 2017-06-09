ee61630c80ae26232e379cd4c2936a9658d6e78b
jake-wickstrom <jtwickstrom@alumni.ubc.ca>
2017-06-09 15:52:26 -0700

    Preliminary Working Fit

--- Simulation/ConstantPowerInput.m
+++ Simulation/ConstantPowerInput.m
@@ -1,70 +1,17 @@
 %% Assign Constants
 BOLTZ = 5.67e-8;
 sensor_loc = [0.03 0.076 0.114 0.190 0.2755]; % (m)
-scaleFactor = [100/1.01984713,0.9782054819,100/0.9905064832,100/0.9990749907,100/1.023384885,100/0.9889810292]; %experimentally determined scaling coefficients
+scaleFactor = [100/1.01984713,100/0.9782054819,100/0.9905064832,100/0.9990749907,100/1.023384885,100/0.9889810292]; %experimentally determined scaling coefficients
 
 %% Import Simulation Data
 uiwait(msgbox('Select a file to import data'))
 [filename,pathname] = uigetfile('*.csv');
 data_record = csvread([pathname,filename]);
 data_record = array2table(data_record,'VariableNames',{'Sensor_1' 'Sensor_2' 'Sensor_3' 'Sensor_4' 'Sensor_5' 'Ambient' 'Time'});
-
-%% Assign Simulation Parameters
-rodlength = .3;
-radius = .01;
-initial_temp = 50 + 273; % °C
-ambient_temp = 20 + 273;  % °K
-input_power = 10;  % Watts
-rod_temp = 20 + 273;     % °K 
-convection_coeff = 5; % convection coefficient
-emissivity = 1;
-
-area = pi*radius^2;     % m^2
-density = 2700;
-heat_cap = 900;
-K = 205;
-
-time = 0;
-
-slice = .03;
-step  = 2;
-
-%% Simulation Setup
-rod_index = linspace(0,rodlength,rodlength/slice + 1);
-rod = ones(1,rodlength/slice + 1)*rod_temp;
-rod(1) = initial_temp;
-
-h = figure;
-plot(rod_index,rod)
-time(1) = 0;
-tracker(1,:) = rod;
-i = 2;
-
-%% Simulation 
-while (time <= max(table2array(data_record(:,'Time'))))
-    time(i) = time(i-1) + step;
-    
-    %calculate power losses at all points along the bar
-    power = -2*pi*radius*slice*convection_coeff*(rod - ambient_temp) - 2*pi*radius*slice*emissivity*BOLTZ*(rod.^4-ambient_temp^4);
-    power(1) = power(1) - area*convection_coeff*(rod(1) - ambient_temp) + input_power;
-    power(end) = power(end) - area*convection_coeff*(rod(end) - ambient_temp);
-    
-    %calculate tempurate difference due to power losses
-    Tloss = power/(heat_cap*density*area*slice);
-    
-    %update temperature along bar
-    rod(2:end-1) = rod(2:end-1) + step*(K/(heat_cap*density))*(rod(1:end-2)-2*rod(2:end-1)+rod(3:end))/slice^2 + step*Tloss(2:end-1);
-    rod(1) = rod(1) + step*(K/(heat_cap*density))*(rod(2)-rod(1))/slice^2 + step*Tloss(1);
-    rod(end) = rod(end) + step*(K/(heat_cap*density))*(rod(end-1)-rod(end))/slice^2 + step*Tloss(end);
-    
-    tracker(i,:) = rod;
-    i = i+1;
-end
-
 %% Plot simulation results against real data
 
 %Create simulation data 
-[Temp , time_array] = TempSim2(9.6 , 1800 , 300);
+[Temp , time_array] = TempSim2(6.5 , floor(max(t)) , 610);
 
 figure
 hold on
@@ -75,7 +22,7 @@ s2 = data_record{:,'Sensor_2'}*scaleFactor(2);
 s3 = data_record{:,'Sensor_3'}*scaleFactor(3);
 s4 = data_record{:,'Sensor_4'}*scaleFactor(4);
 s5 = data_record{:,'Sensor_5'}*scaleFactor(5);
-s6 = data_record{:,'Ambient'};
+s6 = data_record{:,'Ambient'}*100;
 
 %Plot data
 plot(t,s1,t,s2,t,s3,t,s4,t,s5,t,s6)
@@ -83,7 +30,7 @@ plot(t,s1,t,s2,t,s3,t,s4,t,s5,t,s6)
 %Plot Simulation
 plot(time_array,Temp(:,3),time_array,Temp(:,8),time_array,Temp(:,11),time_array,Temp(:,19),time_array,Temp(:,28));
 
-legend('Sensor 1','Sensor 2','Sensor 3','Sensor 4','Sensor 5','Ambient','Sensor 1-SIM','Sensor 2-SIM','Sensor 3-SIM','Sensor 4-SIM','Sensor 5-SIM')
+%legend('Sensor 1','Sensor 2','Sensor 3','Sensor 4','Sensor 5','Ambient','Sensor 1-SIM','Sensor 2-SIM','Sensor 3-SIM','Sensor 4-SIM','Sensor 5-SIM')
 
 %add graph labels
 title('Tempurature of Aluminum Rod with 10W Input')
--- TempSim2.m
+++ TempSim2.m
@@ -1,31 +1,29 @@
 function [ Temp , time_array] = TempSim2( Power , time, frequency )
 %{Pass this function a Power (in Watts) and time and the function will simulate
 %}
-l = 0.3; %Length of rod
+length = 0.3; %Length of rod
 k = 205; %Conductivity - should be 205
 c = 921; %Heat capacity - should be 921
 p = 2700; %density - should be 2700
-r = 0.01 ; %Radius
-k_c = 5; %should be 5
+diameter = .0224;
+r = diameter/2 ; %Radius
+k_c = 6.1; %should be 5
 sigma = 5.67*(10^-8); %Stefan-Boltzman constant
-em = 1; %Emmisivity
+em = .25; %Emmisivity
 
 delta_x = 0.01; %Distance step
-N = l / delta_x; %Total Intervals
-delta_t = 0.1; %Time step
-T_amb = 25; %Ambient Temperature
-T_init = 25; %Initial Temperature
+N = length / delta_x; %Total Intervals
+delta_t = .5; %Time step
+T_amb = 20; %Ambient Temperature
+T_init = 30; %Initial Temperature
 
 T_coeff = delta_t/(c * p * pi * (r^2) * delta_x);
 
-x = linspace(0,l,((l/delta_x))); %Distance vector
+x = linspace(0,length,((length/delta_x))); %Distance vector
 
-Temp = zeros([time ((l/delta_x))]); %Temperature vector
-Temp_change = zeros([1 ((l/delta_x))]); %Temperature change vector
-Temp =Temp +T_amb;
+%Temp = zeros([time*2 ((length/delta_x))]); %Temperature vector
+Temp_change = zeros([1 ((length/delta_x))]); %Temperature change vector
 Temp(1,:) = linspace(T_init , T_init , N);
-
-time_array = linspace(0 , time , time).';
 
 P = Power;
 flag = false;
@@ -35,20 +33,24 @@ if (frequency == 0)
         flag = true;
 end
     
+time_array(1) = 0;
+i=1;
 
-for i = 2:time
+while time_array(end) < time
+    i = i + 1;
+    time_array(i) = time_array(i-1) + delta_t;
     %If power is being modulated, check if power on or off
     if ~flag
-        if (mod(floor(i/frequency)+1,2)== 0)
+        if (mod(floor(time_array(i)/frequency)+1,2)== 0)
         P = 0;
         
-        elseif (mod(floor(i/frequency)+1,1)== 0)
+        elseif (mod(floor(time_array(i)/frequency)+1,1)== 0)
         P = Power;
         end
     end
     
     %Mid points
-    Temp_change(2:N-1) = (k*delta_t/(c*p))*((Temp(i-1,1:N-2)-2*Temp(i-1,2:N-1)+Temp(i-1,3:N))/(delta_x^2));
+    Temp_change(2:N-1) = (k*delta_t/(c*p)).*((Temp(i-1,1:N-2)-2*Temp(i-1,2:N-1)+Temp(i-1,3:N))/(delta_x^2));
         
     %End points
     Temp_change(1) = Temp_change(1) +((-k/(c*p*(delta_x^2)))*(Temp(i-1,1) - Temp(i-1,2)))*(delta_t); %Transfer to next slice, 1 - 2
@@ -57,20 +59,20 @@ for i = 2:time
     Temp_change(1) = Temp_change(1) +((T_coeff)*P); 
     
     %Transfer from slice before
-    Temp_change(30) = (((k/(c*p*(delta_x^2)))*(Temp(i-1,29) - Temp(i-1,30))))*(delta_t); 
+    Temp_change(N) = (((k/(c*p*(delta_x^2)))*(Temp(i-1,N-1) - Temp(i-1,N))))*(delta_t); 
     
     %Loses due to radiation and convection
-    pout_convection = (2*pi*r*delta_x).*(k_c.*(Temp(i-1,:) - T_amb))*0;
+    pout_convection = (2*pi*r*delta_x).*(k_c.*(Temp(i-1,:) - T_amb));
     
-    pout_radiation = (2*pi*r*delta_x) .* em .*(sigma.*  ((Temp(i-1,:)+273).^4-(T_amb+273).^4))*0;
+    pout_radiation = (2*pi*r*delta_x) .* em .*(sigma.*  ((Temp(i-1,:)+273).^4-(T_amb+273).^4));
     
-    Temp_change = Temp_change - ((T_coeff).*(pout_convection + pout_radiation)); %Convective and radiative loss
+    Temp_change = Temp_change - ((T_coeff)*(pout_convection + pout_radiation)); %Convective and radiative loss
      
     Temp(i,:) = Temp_change + Temp(i-1,:);
     
-     if mod(i,100)==0
-        plot(x , Temp(i,:));
-     end
+%      if mod(i,100)==0
+%         plot(x , Temp(i,:));
+%      end
     
 end
 
